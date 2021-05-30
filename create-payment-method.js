c6fb1022e1145415f340f16d05e07a3f0c29afd6
BernardoMB <bmondragonbrozon@gmail.com>
2021-05-30 15:49:47 -0500

    Added function to charge a card for buying a coffee

---
+++ create-payment-method.js
@@ -0,0 +1,76 @@
+const testApiSecretKey = process.env.STRIPE_LIVE_API_KEY;
+const liveApiSecretKey = process.env.STRIPE_TEST_API_KEY;
+const isProduction = false;
+
+const stripe = require('stripe')(isProduction ? liveApiSecretKey : testApiSecretKey);
+
+exports.handler = async (event) => {
+    try {
+        // Create payment method
+        let paymentMethod;
+        try {
+            paymentMethod = await stripe.paymentMethods.create({
+                type: 'card',
+                card: {
+                    number: event.cardNumber,
+                    exp_month: event.cardExpirationMonth,
+                    exp_year: event.cardExpirationYear,
+                    cvc: event.cardSecurityCode
+                },
+            });
+            console.log('Created payment method', paymentMethod);
+        } catch (error) {
+            console.log('Could not create payment method')
+            throw error;
+        }
+        // Create payment intent
+        let paymentIntent;
+        try {
+            const price = 10;
+            if (paymentMethod.id) {
+                paymentIntent = await stripe.paymentIntents.create({
+                    amount: 1 * 100 * price,
+                    currency: 'mxn',
+                    payment_method_types: ['card'],
+                    description: 'Payment intent for buying a coffee', // optional
+                    metadata: {
+                        order: `A cup of coffee $${price}`
+                    },
+                    payment_method: paymentMethod.id,
+                    receipt_email: 'bmondragonbrozon@gmail.com', // optional
+                });
+                console.log('Created payment intent', paymentIntent);
+            } else {
+                throw new Error('This payment flow required a valid payment method id');
+            }
+        } catch (error) {
+            console.log('Could not create payment intent');
+            throw error;
+        }
+        // Confirm
+        try {
+            if (paymentIntent.id) {
+                paymentIntent = await stripe.paymentIntents.confirm(
+                    paymentIntent.id
+                );
+                console.log('Confirmed payment intent', paymentIntent);
+            } else {
+                throw new Error('Cannot confirm payment intent withoud a valid payment intent id');
+            }
+        } catch (error) {
+            console.log('Could not confirm payment intent');
+            throw error;
+        }
+        return {
+            success: true,
+            errorMessage: null
+        };
+    } catch (error) {
+        return {
+            success: false,
+            errorMessage: error.errorMessage
+        };
+    }
+};
+
+//exports.handler();
